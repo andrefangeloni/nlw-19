75dcc540d9c64f2eaea0359b33338e91fb14a93d
André Angeloni <andrefangeloni@gmail.com>
2025-02-24 21:56:34 -0300

    feat: invite page

--- frontend/src/components/icon-button.tsx
+++ frontend/src/components/icon-button.tsx
@@ -1,10 +1,14 @@
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type Props = ComponentProps<'button'>
 
-export const IconButton = (props: Props) => (
+export const IconButton = ({ className, ...props }: Props) => (
   <button
-    className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+    className={twMerge(
+      'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+      className,
+    )}
     {...props}
   />
 )
